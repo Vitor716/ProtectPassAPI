85511bdb451cd2702bacca1fec51f93c080d01e0
Vitor Vieira <vitorvieira_deoliveira@hotmail.com>
2025-01-27 12:33:52 -0300

    Commit inicial

---
+++ src/main/java/org/example/protecpassapi/ProtecPassApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.protecpassapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtecPassApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtecPassApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/protecpassapi/ProtecPassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.protecpassapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtecPassApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
